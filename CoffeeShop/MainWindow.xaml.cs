594ef112073b695fa4e3a0bb9b7bd6d15bc05a7f
Denzyl Soobramoney Govender <135228226+Denzylsgovender@users.noreply.github.com>
2025-03-05 05:27:21 +0200

    Add files via upload

---
+++ CoffeeShop/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows;
+
+namespace CoffeeShop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrderCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            string coffeeType = cmbCoffeeType.Text;
+            string order = "Ordered: " + coffeeType;
+
+            if (chkMilk.IsChecked == true)
+            {
+                order += " + Milk";
+            }
+
+            if (chkSugar.IsChecked == true)
+            {
+                order += " + Sugar";
+            }
+
+            txtOrderSummary.Text = order;
+        }
+    }
+}
